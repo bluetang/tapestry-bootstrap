f1f502e86fe1fbe4db3942fbac6b415b39599ac1
yunglin <yunglin@gmail.com>
2012-02-02 19:37:16 +0800

    fixed modal component.

--- src/main/java/com/trsvax/bootstrap/components/Modal.java
+++ src/main/java/com/trsvax/bootstrap/components/Modal.java
@@ -1,20 +1,24 @@
 package com.trsvax.bootstrap.components;
 
+import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.Block;
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 
+
 /**
- * Modal dialog
+ * Modal dialog. To use enable the modal, you have to place a control element on the page.
+ * <pre><code><a class="btn" data-controls-modal="id" data-backdrop="true">Add New Location</a></code></pre>
+ * 
  * @tapestrydoc
  * @see <a href="http://twitter.github.com/bootstrap/#popovers">Bootstrap Popovers Modals</a>
  */
 @SuppressWarnings("unused")
+@Import(library="classpath:/com/trsvax/bootstrap/bootstrap-modal.js")
 public class Modal extends BootstrapComponent {
 
-
-	@Parameter
-	@Property
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
 	private String id;
 
 	@Parameter
@@ -24,4 +28,5 @@ public class Modal extends BootstrapComponent {
 	@Parameter
 	@Property
 	private Block footer;
+
 }
